a9531d7412c1c91d9e69ce33dbcd7be267ba35c7
thepaperpilot <thepaperpilot@gmail.com>
2014-11-20 13:04:00 -0600

    Renamed class

--- src/BinomialModel.java
+++ src/BinomialModel.java
@@ -2,7 +2,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Binomial_Model {
+public class BinomialModel {
 
     public static double success_probability;
 
