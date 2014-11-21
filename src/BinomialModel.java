88b03c6305df77f0c22fff1b9f02b572c0aa9801
thepaperpilot <thepaperpilot@gmail.com>
2014-11-20 18:12:57 -0600

    Formatted everything how I wanted it, and finished the GUI

--- src/BinomialModel.java
+++ src/BinomialModel.java
@@ -1,125 +1,79 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-public class BinomialModel {
+class BinomialModel {
 
-    public static double success_probability;
-    public static int trials;
+public static double success_probability;
+public static int trials;
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("What is the probability of success?");
-        success_probability = s.nextDouble();
-        System.out.println("How many trials are done?");
-        trials = s.nextInt();
-        s.nextLine();
-        ArrayList<BigDecimal> occurrence_probability = new ArrayList<BigDecimal>();
-        ArrayList<String> occurrence_n = new ArrayList<String>();
-        for (int i = 0; i <= trials; i++) {
-            occurrence_probability.add(individual_probability(trials, i));
-            if (i>9){
-                occurrence_n.add("     " + i + "     ");
-            }else occurrence_n.add("     " + i + "      ");
-        }
-        BigDecimal expected_number_of_results = BigDecimal.valueOf(trials*success_probability);
-        BigDecimal standard_deviation = BigDecimal.valueOf(Math.pow(trials*success_probability*(1-success_probability),.5)).setScale(10,6);
-        while (true) {
-            System.out.println("What more information do you need?");
-            String info = s.nextLine();
-            if(info.equalsIgnoreCase("end")){
-                break;
+private static BigDecimal individual_probability(int n, int s) {
+    return nPk(n, s).multiply(BigDecimal.valueOf(success_probability).pow(s)).multiply(BigDecimal.valueOf(1).subtract(BigDecimal.valueOf(success_probability)).pow(n - s)).setScale(10, 6);
+}
+
+private static BigDecimal nPk(int n, int k){
+    if(n==k||k==0){
+        return BigDecimal.ONE;
+    }
+    if(k==1||k==n-1){
+        return BigDecimal.valueOf(n);
+    }
+    return factorial(n).divide(factorial(k).multiply(factorial(n-k)),10,6);
+}
+
+private static BigDecimal factorial(int n){
+    BigDecimal factorial = BigDecimal.valueOf(n);
+    while(n>2) {
+        n--;
+        factorial = factorial.multiply(BigDecimal.valueOf(n));
+    }
+    return factorial;
+}
+
+public static String getStandardDeviation() {
+    return BigDecimal.valueOf(Math.pow(trials * success_probability * (1 - success_probability), .5)).setScale(10, 6).toPlainString();
+}
+
+public static String getNumberResults() {
+    return BigDecimal.valueOf(trials * success_probability).toPlainString();
+}
+
+public static String getProbabilityDistribution() {
+    String out = "";
+    for(int i = 0; i <= trials; i++) {
+        out += i + ": " + individual_probability(trials, i) + "\n";
+    }
+    return out;
+}
+
+public static String getSNumber(int selectedIndex, int sNum) {
+    BigDecimal answer = BigDecimal.ZERO;
+    ArrayList<BigDecimal> occurrences = new ArrayList<BigDecimal>();
+    for (int i = 0; i <= trials; i++) {
+        occurrences.add(individual_probability(trials, i));
+    }
+    switch (selectedIndex) {
+        case 0: // <=
+            for (int i = 0; i <= sNum; i++) {
+                answer = answer.add(occurrences.get(i));
             }
-            BigDecimal answer = BigDecimal.ZERO;
-            int number = 0;
-            if (info.length()<=4) {
-                number = Integer.valueOf(Character.toString(info.charAt(info.length()-1)));
-            } else if(info.length()==5){
-                number = Integer.valueOf(info.substring(info.length()-2,info.length()));
+            break;
+        case 1: // >=
+            for (int i = sNum; i <= trials; i++) {
+                answer = answer.add(occurrences.get(i));
             }
-            if (info.contains("standard")|| info.contains("deviation")){
-                System.out.println(standard_deviation);
-            }else if (info.contains("expected")|| info.contains("number")||info.contains("of")|| info.contains("results")){
-                System.out.println(expected_number_of_results);
-            }else if (info.contains("probability")|| info.contains("distribution")){
-                System.out.println(occurrence_n);
-                System.out.println(occurrence_probability);
-            }else if (info.contains("s>=")) {
-                for (int i = number; i <= trials; i++) {
-                    answer = answer.add(occurrence_probability.get(i));
-                }
-                System.out.println(answer);
-            } else if (info.contains("s>")) {
-                if (info.length()<=3) {
-                    number = Integer.valueOf(Character.toString(info.charAt(info.length()-1)));
-                } else if(info.length()==4){
-                    number = Integer.valueOf(info.substring(info.length()-2,info.length()));
-                }
-                for (int i = number+1; i <= trials; i++) {
-                    answer = answer.add(occurrence_probability.get(i));
-                }
-                System.out.println(answer);
-            } else if (info.contains("s=")) {
-                if (info.length()<=3) {
-                    number = Integer.valueOf(Character.toString(info.charAt(info.length()-1)));
-                } else if(info.length()==4){
-                    number = Integer.valueOf(info.substring(info.length()-2,info.length()));
-                }
-                System.out.println(occurrence_probability.get(number));
-            }else if (info.contains("s<=")) {
-                for (int i = 0; i <= number; i++) {
-                    answer = answer.add(occurrence_probability.get(i));
-                }
-                System.out.println(answer);
-            }else if (info.contains("s<")) {
-                if (info.length()<=3) {
-                    number = Integer.valueOf(Character.toString(info.charAt(info.length()-1)));
-                } else if(info.length()==4){
-                    number = Integer.valueOf(info.substring(info.length()-2,info.length()));
-                }
-                for (int i = 0; i < number; i++) {
-                    answer = answer.add(occurrence_probability.get(i));
-                }
-                System.out.println(answer);
+            break;
+        case 2: // <
+            for (int i = 0; i < sNum; i++) {
+                answer = answer.add(occurrences.get(i));
             }
-        }
+            break;
+        case 3: // >
+            for (int i = sNum + 1; i <= trials; i++) {
+                answer = answer.add(occurrences.get(i));
+            }
+            break;
     }
-
-    public static BigDecimal individual_probability(int n, int s) {
-        return nPk(n, s).multiply(BigDecimal.valueOf(success_probability).pow(s)).multiply(BigDecimal.valueOf(1).subtract(BigDecimal.valueOf(success_probability)).pow(n - s)).setScale(10, 6);
-    }
-
-    public static BigDecimal nPk(int n, int k){
-        if(n==k||k==0){
-            return BigDecimal.ONE;
-        }
-        if(k==1||k==n-1){
-            return BigDecimal.valueOf(n);
-        }
-        return factorial(n).divide(factorial(k).multiply(factorial(n-k)),10,6);
-    }
-
-    public static BigDecimal factorial(int n){
-        BigDecimal factorial = BigDecimal.valueOf(n);
-        while(n>2) {
-            n--;
-            factorial = factorial.multiply(BigDecimal.valueOf(n));
-        }
-        return factorial;
-    }
-
-    public static String calc() {
-        ArrayList<BigDecimal> occurrence_probability = new ArrayList<BigDecimal>();
-        ArrayList<String> occurrence_n = new ArrayList<String>();
-        for (int i = 0; i <= trials; i++) {
-            occurrence_probability.add(individual_probability(trials, i));
-            if (i>9){
-                occurrence_n.add("     " + i + "     ");
-            }else occurrence_n.add("     " + i + "      ");
-        }
-        return "Standard Deviation: " + BigDecimal.valueOf(Math.pow(trials*success_probability*(1-success_probability),.5)).setScale(10,6) + "\n" +
-                "Expected Number Of Results: " + BigDecimal.valueOf(trials*success_probability) + "\n" +
-/*TODO Format better*/ "Probability Distribution: " + occurrence_n + "\n" + occurrence_probability;
-    }
+    return answer.toPlainString();
+}
 }
 
--- src/GUI.java
+++ src/GUI.java
@@ -1,36 +1,61 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI {
-    private JTextField probability;
-    private JTextField trials;
-    private JTextPane out;
-    public JPanel panel1;
+class GUI {
+private JTextField probability;
+private JTextField trials;
+private JPanel panel1;
+private JButton updateButton;
+private JTextField standardDeviation;
+private JTextField numResults;
+private JTextPane probDist;
+private JScrollPane probDistPane;
+private JComboBox compare;
+private JSpinner sNum;
+private JTextField sOut;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("GUI");
-        frame.setContentPane(new GUI().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
+public static void main(String[] args) {
+    JFrame frame = new JFrame("GUI");
+    frame.setContentPane(new GUI().panel1);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+}
 
-    public GUI() {
-        probability.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                BinomialModel.success_probability = Double.parseDouble(((JTextField) e.getSource()).getText());
-                out.setText(BinomialModel.calc());
-            }
-        });
-        trials.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                BinomialModel.trials = Integer.parseInt(((JTextField) e.getSource()).getText());
-                out.setText(BinomialModel.calc());
-            }
-        });
-    }
+private GUI() {
+    updateButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            BinomialModel.success_probability = Double.parseDouble(probability.getText());
+            BinomialModel.trials = Integer.parseInt(trials.getText());
+            standardDeviation.setText(BinomialModel.getStandardDeviation());
+            numResults.setText(BinomialModel.getNumberResults());
+            sOut.setText(BinomialModel.getSNumber(compare.getSelectedIndex(), (Integer) sNum.getValue()));
+            probDist.setText(BinomialModel.getProbabilityDistribution());
+            probDistPane.getVerticalScrollBar().setValue(0);
+        }
+    });
+    compare.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            sOut.setText(BinomialModel.getSNumber(compare.getSelectedIndex(), (Integer) sNum.getValue()));
+        }
+    });
+    sNum.addChangeListener(new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent changeEvent) {
+            sOut.setText(BinomialModel.getSNumber(compare.getSelectedIndex(), (Integer) sNum.getValue()));
+        }
+    });
 
+    probability.setBorder(BorderFactory.createLineBorder(new Color(187, 187, 187)));
+    trials.setBorder(BorderFactory.createLineBorder(new Color(187, 187, 187)));
+    standardDeviation.setBorder(BorderFactory.createEmptyBorder());
+    numResults.setBorder(BorderFactory.createEmptyBorder());
+    sOut.setBorder(BorderFactory.createEmptyBorder());
+}
 }
