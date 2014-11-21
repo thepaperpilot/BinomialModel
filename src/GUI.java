3f57e326863b29680f630239667992492367877a
thepaperpilot <thepaperpilot@gmail.com>
2014-11-21 10:47:53 -0600

    Set a new minimum window size, because the values didn't work after testing on windows. Gave the minimum some extra buffer, so it won't need to be changed again

--- src/GUI.java
+++ src/GUI.java
@@ -23,7 +23,7 @@ public static void main(String[] args) {
     JFrame frame = new JFrame("GUI");
     frame.setContentPane(new GUI().panel1);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setMinimumSize(new Dimension(374, 344));
+    frame.setMinimumSize(new Dimension(400, 400));
     frame.pack();
     frame.setVisible(true);
 }
